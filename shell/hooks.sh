83acab4c8ab63272e94a8acb3dcaa03de6bd0738
Chandler DeLoach <deloach.cdd@gmail.com>
2019-09-06 23:00:35 -0600

    push-up tweaking

--- shell/hooks.sh
+++ shell/hooks.sh
@@ -6,20 +6,16 @@ if [[ ! -e "$BASH_CONFIG_DIR" ]]; then
 	mkdir -p "$BASH_CONFIG_DIR"
 fi
 
-# Copy primary bash dotfiles to home directory
-deploy_dotfile "$PWD/dotfiles/profile" "$BASH_CONFIG_DIR"
 if [[ ! -e "$BASH_CONFIG_DIR/bash" ]]; then
     mkdir "$BASH_CONFIG_DIR/bash"
 fi
-deploy_dotfile "$PWD/dotfiles/bash/init.sh" \
-    "$BASH_CONFIG_DIR/bash/init.sh"
-deploy_dotfile "$PWD/dotfiles/bash/aliases.sh" \
-    "$BASH_CONFIG_DIR/bash/aliases.sh"
-if [[ ! -e "$BASH_CONFIG_DIR/bash/distro-defaults" ]]; then
-    mkdir "$BASH_CONFIG_DIR/bash/distro-defaults"
+deploy_dotfile "$PWD/dotfiles/bash/init.sh" "$BASH_CONFIG_DIR/init.sh"
+deploy_dotfile "$PWD/dotfiles/bash/aliases.sh" "$BASH_CONFIG_DIR/aliases.sh"
+if [[ ! -e "$BASH_CONFIG_DIR/distro-defaults" ]]; then
+    mkdir "$BASH_CONFIG_DIR/distro-defaults"
 fi
 cp "$PWD/dotfiles/bash/distro-defaults/debian" \
-    "$BASH_CONFIG_DIR/bash/distro-defaults/debian"
+    "$BASH_CONFIG_DIR/distro-defaults/debian"
 
 # Copy shell-agnostic profile to home directory
 cp "$PWD/dotfiles/profile" "$HOME/.profile"
