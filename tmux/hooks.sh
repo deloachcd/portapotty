8c38ddd828cd84b7e4dbd1d3d4178ea82ee575c6
Chandler DeLoach <deloach.cdd@gmail.com>
2019-09-06 22:49:39 -0600

    added tmux support

--- tmux/hooks.sh
+++ tmux/hooks.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
+source ../pottyfunctions.sh
 
+TMUX_CONFIG_DIR="$HOME/.config/tmux"
+if [[ ! -e "$TMUX_CONFIG_DIR" ]]; then
+    mkdir -p "$TMUX_CONFIG_DIR"
+fi
+
+cp ./dotfiles/tmuxline_seoul256 "$TMUX_CONFIG_DIR"
+deploy_dotfile ./dotfiles/tmux.conf "$TMUX_CONFIG_DIR"
+ln -sf "$TMUX_CONFIG_DIR/tmux.conf" "$HOME/.tmux.conf"
