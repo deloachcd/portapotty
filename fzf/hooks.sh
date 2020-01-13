b392ddefb0621610aeb4ecfc6903dbafe7385a9f
Chandler DeLoach <deloach.cdd@gmail.com>
2020-01-13 11:51:48 -0700

    update fzf deploy

--- fzf/hooks.sh
+++ fzf/hooks.sh
@@ -3,8 +3,5 @@ source ../pottyfunctions.sh
 
 FZF_VERSION=0.18.0
 FZF_URL="https://github.com/junegunn/fzf-bin/releases/download/$FZF_VERSION/fzf-$FZF_VERSION-linux_amd64.tgz"
-FLAGS="$@"
 
-if [[ "$FLAGS" =~ "full" ]]; then
-    wget -c "$FZF_URL" -O - | tar -xzv -C $HOME/.local/bin
-fi
+wget -c "$FZF_URL" -O - | tar -xzv -C $HOME/.local/bin
