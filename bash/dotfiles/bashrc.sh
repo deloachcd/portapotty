e5e3ac62a284c4293cad383a632347c6a26590ab
Chandler DeLoach <deloach.cdd@gmail.com>
2020-01-13 16:48:53 -0700

    ensure user bin is sourced in bashrc

--- bash/dotfiles/bashrc.sh
+++ bash/dotfiles/bashrc.sh
@@ -6,3 +6,8 @@ source $BASH_CONFIG_DIR/aliases.sh
 if [[ -e $BASH_CONFIG_DIR/local.sh ]]; then
     source $BASH_CONFIG_DIR/local.sh
 fi
+
+# Ensure user bin is in PATH
+if [ -d "$HOME/.local/bin" ] ; then
+    PATH="$HOME/.local/bin:$PATH"
+fi
