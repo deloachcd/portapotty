0a59a51e898d6213c4e8321b1bdeec9e6697bf25
Chandler DeLoach <deloach.cdd@gmail.com>
2020-01-13 16:44:25 -0700

    add local script source to bashrc

--- bash/dotfiles/bashrc.sh
+++ bash/dotfiles/bashrc.sh
@@ -2,3 +2,7 @@ export BASH_CONFIG_DIR=~/.config/bash
 
 source $BASH_CONFIG_DIR/defaults.sh
 source $BASH_CONFIG_DIR/aliases.sh
+
+if [[ -e $BASH_CONFIG_DIR/local.sh ]]; then
+    source $BASH_CONFIG_DIR/local.sh
+fi
