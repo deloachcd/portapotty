3bf5ebdd66bf1d7316b8d883232131aeb51fb6c7
Chandler DeLoach <deloach.cdd@gmail.com>
2021-02-18 10:43:09 -0700

    placeholder hooks so glfw-provisioning dir doesn't break deploy

--- glfw-provisioning/install-glfw3.sh
+++ glfw-provisioning/install-glfw3.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# This script is designed to provision GLFW3 C++ projects, to run
-# on Ubuntu 20.04.
+# This script is designed to install GLFW3 for C++ projects development
+# to run on Ubuntu 20.04.
 #
 # Ensure that /etc/apt/sources.list has "deb-src" lines commented
 # out before running this, so that build dependencies for GLFW3
