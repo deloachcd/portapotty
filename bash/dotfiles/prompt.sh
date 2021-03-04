d39c039595eda91f63acd6c36f76d87814fae6f4
Chandler DeLoach <deloach.cdd@gmail.com>
2021-03-04 11:39:43 -0700

    automagically folding bash prompt, like in emacs' eshell

--- bash/dotfiles/prompt.sh
+++ bash/dotfiles/prompt.sh
@@ -1,3 +1,30 @@
+function __folding_prompt {
+    # Have the bash prompt automagically fold long
+    # paths, like in emacs' eshell
+    pwd | gawk -F '/' '{
+        NFF=NF-2
+        for(i=2; i<=NF; i++) {
+            if (i == 2 && $i == "home") {
+                if (NF == 3) {
+                    printf "~"
+                } else {
+                    printf "~/"
+                }   
+                i++
+            } else if (i == NF) {
+                printf $i
+            } else {
+                if (NF > 4 && i < NFF) {
+                    printf substr($i, 0, 1)"/"
+                } else {
+                    printf $i"/"
+                }
+            }
+        }
+        printf "\n"
+    }'
+}
+
 # assemble the prompt string PS1
 # ripped off from: https://stackoverflow.com/a/16715681
 function __build_prompt {
@@ -13,7 +40,7 @@ function __build_prompt {
         PS1+="${FG_RED}${EXIT}${FG_RESET} " # Add red if exit code non 0
     fi
     # this is the default prompt for 
-    PS1+="${BOLD_GRAY}\w ${RESET}\$ "
+    PS1+="${BOLD_GRAY}$(__folding_prompt) ${RESET}\$ "
 }
 
 PROMPT_COMMAND=__build_prompt
