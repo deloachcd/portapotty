9d05064c27d6fe601c77eaae1e5a65c3b99917c9
Chandler DeLoach <deloach.cdd@gmail.com>
2021-03-11 12:18:11 -0700

    profile now deployed from zsh potty

--- bash/hooks.sh
+++ bash/hooks.sh
@@ -4,7 +4,6 @@ if [[ ! -e "$BASH_CONFIG_DIR" ]]; then
 fi
 
 deploy_dotfile "$PWD/dotfiles/bashrc" "$HOME/.bashrc"
-deploy_dotfile "$PWD/dotfiles/profile.sh" "$HOME/.profile"
 deploy_dotfile "$PWD/dotfiles/constants.sh" "$BASH_CONFIG_DIR/constants.sh"
 deploy_dotfile "$PWD/dotfiles/path.sh" "$BASH_CONFIG_DIR/path.sh"
 deploy_dotfile "$PWD/dotfiles/aliases.sh" "$BASH_CONFIG_DIR/aliases.sh"
