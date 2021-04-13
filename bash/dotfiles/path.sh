150951926c859bb24422abccecb9069a6d368580
Chandler DeLoach <deloach.cdd@gmail.com>
2021-04-13 11:56:55 -0600

    finally deprecate bash logic deployment

--- bash/dotfiles/path.sh
+++
@@ -1,4 +0,0 @@
-if [[ -d "$HOME/.local/bin" && \
-	! "$PATH" == *"$HOME/.local/bin"* ]] ; then
-    PATH="$HOME/.local/bin:$PATH"
-fi
--- bash/hooks.sh
+++
@@ -1,9 +0,0 @@
-BASH_CONFIG_DIR="$HOME/.config/bash"
-if [[ ! -e "$BASH_CONFIG_DIR" ]]; then
-    mkdir -p "$BASH_CONFIG_DIR"
-fi
-
-deploy_dotfile "$PWD/dotfiles/bashrc" "$HOME/.bashrc"
-deploy_dotfile "$PWD/dotfiles/constants.sh" "$BASH_CONFIG_DIR/constants.sh"
-deploy_dotfile "$PWD/dotfiles/path.sh" "$BASH_CONFIG_DIR/path.sh"
-deploy_dotfile "$PWD/dotfiles/aliases.sh" "$BASH_CONFIG_DIR/aliases.sh"
