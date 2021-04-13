2771cc3b5d8b3109d572357e8fd9d15a5021a4f5
Chandler DeLoach <deloach.cdd@gmail.com>
2021-04-13 13:05:40 -0600

    fix type in konsole hooks

--- konsole/hooks.sh
+++ konsole/hooks.sh
@@ -1,4 +1,4 @@
-KONSOLE_DIR=$HOME/.local/share/konsole
+KONSOLE_DIR="$HOME/.local/share/konsole"
 
 ensure_dir_exists "$KONSOLE_DIR"
-deploy_dotfile "$PWD/dotfiles/nord.colorscheme" "$LOCAL_INSTALL_DIR/nord.colorscheme"
+deploy_dotfile "$PWD/dotfiles/nord.colorscheme" "$KONSOLE_DIR/nord.colorscheme"
