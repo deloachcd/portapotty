018b978bc6c9101b6ad9e570aa27016ca7f06498
Chandler DeLoach <deloach.cdd@gmail.com>
2021-04-13 14:01:28 -0600

    switch zsh to symlink paradigm

--- zsh/hooks.sh
+++ zsh/hooks.sh
@@ -1,12 +1,5 @@
-ZSH_CONFIG_DIR="$HOME/.config/zsh"
-ensure_dir_exists "$ZSH_CONFIG_DIR"
-
-deploy_dotfile "$PWD/dotfiles/zshrc" "$HOME/.zshrc"
-deploy_dotfile "$PWD/dotfiles/constants.sh" "$ZSH_CONFIG_DIR/constants.sh"
-deploy_dotfile "$PWD/dotfiles/prompt.sh" "$ZSH_CONFIG_DIR/prompt.sh"
-deploy_dotfile "$PWD/dotfiles/path.sh" "$ZSH_CONFIG_DIR/path.sh"
-deploy_dotfile "$PWD/dotfiles/aliases.sh" "$ZSH_CONFIG_DIR/aliases.sh"
-deploy_dotfile "$PWD/dotfiles/bsd-style-pkg.sh" "$ZSH_CONFIG_DIR/pkg.sh"
+link_config "$PWD/home/zshrc" "$HOME/.zshrc"
+link_config "$PWD/config" "$HOME/.config/zsh"
 
 # change user's shell to zsh if it's bash
 if cat /etc/passwd | grep $(whoami) | grep bash 2>&1 1>/dev/null; then
