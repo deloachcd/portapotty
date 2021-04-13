25a3cd29d590b1104df34f725c89ab4ba03caf38
Chandler DeLoach <deloach.cdd@gmail.com>
2021-04-13 13:41:43 -0600

    switch to new paradigm for konsole

--- konsole/hooks.sh
+++ konsole/hooks.sh
@@ -1,4 +1,4 @@
 KONSOLE_DIR="$HOME/.local/share/konsole"
 
 ensure_dir_exists "$KONSOLE_DIR"
-deploy_dotfile "$PWD/dotfiles/nord.colorscheme" "$KONSOLE_DIR/nord.colorscheme"
+link_config "$PWD/share/nord.colorscheme" "$KONSOLE_DIR/nord.colorscheme"
