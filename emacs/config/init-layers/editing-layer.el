d9655f68cf3605236fa51473b0aab7299f2ebef0
Chandler DeLoach <deloach.cdd@gmail.com>
2021-08-03 20:21:05 -0600

    add witchmacs-inspired editor config changes

--- emacs/config/init-layers/editing-layer.el
+++ emacs/config/init-layers/editing-layer.el
@@ -1,4 +1,10 @@
 (provide 'editing-layer)
+;; This is the primary configuration layer for customizing how emacs
+;; behaves as a text editor, mainly through tweaking its own defaults
+;; and evil-mode.
+;;
+;; Much of the ideas here are taken from Witchmacs, by snackon:
+;; https://github.com/snackon/Witchmacs
 
 ;; Vim-like bindings everywhere
 (use-package evil
@@ -19,6 +25,35 @@
 (add-hook 'text-mode-hook (lambda ()
 			    (hl-line-mode)))
 
+;; Indentation
+(setq-default tab-width 4)
+(setq-default standard-indent 4)
+(setq c-basic-offset tab-width)
+(setq-default electric-indent-inhibit t)
+(setq-default indent-tabs-mode t)
+(setq backward-delete-char-untabify-method 'nil)
+
 ;; Don't litter every working directory with backups
 (defvar backup-dir "~/.emacs.d/backups")
 (setq backup-directory-alist (list (cons "." backup-dir)))
+
+;; Copypasting outside of emacs
+(setq x-select-enable-clipboard t)
+
+;; Disable ring-bell
+(setq ring-bell-function 'ignore)
+
+;; Prettify symbols globally
+(global-prettify-symbols-mode t)
+
+;; Bracket pair-matching
+(setq electric-pair-pairs '(
+			    (?\{ . ?\})
+			    (?\( . ?\))
+			    (?\[ . ?\])
+			    (?\" . ?\")
+			    ))
+(electric-pair-mode t)
+
+;; Type "y" or "n" instead of "yes" or "no" in minibuffer
+(defalias 'yes-or-no-p 'y-or-n-p)
