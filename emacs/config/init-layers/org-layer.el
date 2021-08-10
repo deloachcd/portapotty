8ebba5bf24b5974e3b73aea71eb25c24621e273e
Chandler DeLoach <deloach.cdd@gmail.com>
2021-08-10 10:35:07 -0600

    Make embedded source code in org-mode display properly

--- emacs/config/init-layers/org-layer.el
+++ emacs/config/init-layers/org-layer.el
@@ -18,6 +18,13 @@
 
 (setq org-agenda-files (concat org-root "/agenda"))
 
+;; Hopefully, fix messed up indentation in source code blocks
+(setq org-src-fontify-natively t
+      org-src-window-setup 'current-window
+      org-src-strip-leading-and-trailing-blank-lines t
+      org-src-preserve-indentation t
+      org-src-tab-acts-natively t)
+
 ;; keybinds
 (general-create-definer org-bindings
   :prefix "SPC o"
@@ -25,19 +32,19 @@
   :keymaps 'override)
 
 (org-bindings
- ;; insertion
- "i i" 'org-insert-item
- "i h" 'org-insert-heading
- "i s" 'org-insert-subheading
- "i l" 'org-insert-link
- ;; org-roam
- "r f" 'org-roam-node-find
- "r i" 'org-roam-node-insert
- "r b" 'org-roam-buffer-toggle
- "r c" 'org-capture-finalize
- ;; following links
- "l n" 'org-open-at-point
- "l p" 'org-mark-ring-goto
- ;; checking boxes
- "t b" 'org-toggle-checkbox
- )
+  ;; insertion
+  "i i" 'org-insert-item
+  "i h" 'org-insert-heading
+  "i s" 'org-insert-subheading
+  "i l" 'org-insert-link
+  ;; org-roam
+  "r f" 'org-roam-node-find
+  "r i" 'org-roam-node-insert
+  "r b" 'org-roam-buffer-toggle
+  "r c" 'org-capture-finalize
+  ;; following links
+  "l n" 'org-open-at-point
+  "l p" 'org-mark-ring-goto
+  ;; checking boxes
+  "t b" 'org-toggle-checkbox
+  )
