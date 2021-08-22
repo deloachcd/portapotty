34cfae72d093c8dfd641cd36db805defc4632550
Chandler DeLoach <deloach.cdd@gmail.com>
2021-08-21 22:38:30 -0600

    Fine tune lsp-ui-doc dialog behavior + add keybind for toggling

--- emacs/config/init-layers/ide-layer.el
+++ emacs/config/init-layers/ide-layer.el
@@ -1,10 +1,15 @@
 (provide 'ide-layer)
 
+
 ;; Primary autocompletion engine
 (use-package company)
 (use-package lsp-mode :commands lsp)
-(use-package lsp-ui :commands lsp-ui-mode)
-
+(use-package lsp-ui
+  :hook (lsp-ui-mode . (lambda () (progn
+                               (setq lsp-ui-doc-enable nil)
+                               (setq lsp-ui-doc-position 'bottom)
+                               (setq lsp-ui-doc-alignment 'window))))
+  :commands lsp-ui-mode)
 
 ;; elisp specific configuration
 (add-hook 'emacs-lisp-mode-hook 'electric-pair-local-mode)
@@ -21,3 +26,19 @@
 
 (add-hook 'c-mode-hook 'c-mode-hook-additions)
 (add-hook 'c++-mode-hook 'c-mode-hook-additions)
+
+;; Python specific configuration
+(defun python-mode-hook-additions ()
+  (electric-pair-local-mode)
+  (lsp))
+
+(add-hook 'python-mode-hook 'python-mode-hook-additions)
+
+;; LSP keybinds
+(general-create-definer lsp-bindings
+  :prefix "SPC l"
+  :states '(normal emacs)
+  :keymaps 'override)
+
+(lsp-bindings
+  "d" 'lsp-ui-doc-mode)
