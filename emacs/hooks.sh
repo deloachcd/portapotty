8ddff45ba3f8e289547085415287fcec880e8386
Chandler DeLoach <deloach.cdd@gmail.com>
2021-11-10 21:46:29 -0700

    bump config rev

--- emacs/hooks.sh
+++ emacs/hooks.sh
@@ -1,3 +1,9 @@
+cd config
+git submodule init
+git submodule update
+cd ..
+
 link_config chemacs2 ~/.emacs.d
 link_config config/emacs-profiles.el ~/.emacs-profiles.el
 link_config config/doom ~/.config/doom
+
