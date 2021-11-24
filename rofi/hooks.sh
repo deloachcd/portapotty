5a7f189ed9f0630846d745a422b142dfe1922715
Chandler DeLoach <deloach.cdd@gmail.com>
2021-11-23 21:17:08 -0700

    want "run" options beneath drun options in rofi

--- rofi/hooks.sh
+++ rofi/hooks.sh
@@ -1,4 +1,4 @@
 # Bind Alt+SPC to this command in shortcuts:
-# rofi -modi combi -show combi -combi-modi window,drun
+# rofi -modi combi -show combi -combi-modi window,drun,run
 
 link_config config ~/.config/rofi
