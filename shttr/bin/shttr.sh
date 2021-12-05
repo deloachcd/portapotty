fa60faea6fa486537a92af9ee01f9a97d974ab83
Chandler DeLoach <deloach.cdd@gmail.com>
2021-12-04 18:26:50 -0700

    moving closer to stable version of shttr

--- shttr/bin/shttr.sh
+++ shttr/bin/shttr.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -e
 
 #             mm
 #             ##          ##        ##
@@ -9,9 +9,6 @@
 #    """"""   ""    ""     """"      """"    ""
 #
 #  "...because the AUR is overcomplicated S-H-I-T!"
-
-# run in bash 'strict mode'
-set -euo pipefail
 
 SHTTR_HOME=$HOME/.local/share/shttr
 
@@ -52,22 +49,70 @@ EOF
 }
 
 _get_debfiles() {
+    # NOTE: wildcard $TARGET*.deb extraction/deletion isn't very clean, but
+    # I don't know if 'apt download' has an output flag that makes using it
+    # unnecessary
     local TARGET="$1"
     apt download "$TARGET"
     dpkg --extract $TARGET*.deb ${TARGET}_debfiles
+    rm $TARGET*.deb
 }
 
 _make_package() {
     local SHTTR_PACKAGE_NAME="$1"
-    mkdir "$SHTTR_PACKAGE_NAME"
-    cp $SHTTR_HOME/templates/recipe.sh "$SHTTR_PACKAGE_NAME/recipe.sh"
-    cp $SHTTR_HOME/templates/Makefile "$SHTTR_PACKAGE_NAME/Makefile"
-    cp $SHTTR_HOME/templates/shttr.conf "$SHTTR_PACKAGE_NAME/shttr.conf"
+    # Create directory for package listing
+    echo "Creating package listing for '$SHTTR_PACKAGE_NAME' in '$SHTTR_HOME/pkgs'"
+    mkdir "$SHTTR_HOME/pkgs/$SHTTR_PACKAGE_NAME"
+    cd "$SHTTR_HOME/pkgs/$SHTTR_PACKAGE_NAME"
+
+    # Copy over initial templates
+    cp $SHTTR_HOME/templates/recipe.sh .
+    cp $SHTTR_HOME/templates/Makefile .
+    cp $SHTTR_HOME/templates/shttr.conf .
+
+    # Check if package exists in APT, and we can look for desktop files/icons there
+    if apt-cache show "$SHTTR_PACKAGE_NAME" 1>/dev/null 2>&1; then
+        # Package exists in APT, pull archive and look for anything useful
+        echo "Package exists in apt, pulling .deb archive to look for desktop files and icons"
+        _get_debfiles "${SHTTR_PACKAGE_NAME}"
+        cd ${SHTTR_PACKAGE_NAME}_debfiles
+
+        # Pull desktop files (or write generic template)
+        if [[ -e usr/share/applications/${SHTTR_PACKAGE_NAME}.desktop ]]; then
+            echo "Hit ${SHTTR_PACKAGE_NAME}.desktop, copying it"
+            mv usr/share/applications/${SHTTR_PACKAGE_NAME}.desktop \
+                ../${SHTTR_PACKAGE_NAME}.desktop
+        elif [[ -d usr/share/applications/ ]]; then
+            echo "Hit /usr/share/applications directory, copying as applications/"
+            mv usr/share/applications ..
+        else
+            echo "Couldn't find any desktop files, copying generic template as default.desktop"
+            cp ../../../templates/default.desktop ..
+        fi
+
+        # Pull icons (or write generic one)
+        if [[ -d usr/share/pixmaps/ ]]; then
+            echo "Found /usr/share/pixmaps directory, copying it"
+            mv usr/share/pixmaps ..
+        else
+            echo "Couldn't find any pixmaps, copying generic 256x256 icon into pixmaps/"
+            mkdir ../pixmaps
+            cp ../../../templates/shttr.png ../pixmaps/shttr.png
+        fi
+
+        cd ..
+        rm -r ${SHTTR_PACKAGE_NAME}_debfiles
+    else
+        echo "Couldn't find any desktop files, copying generic template as default.desktop"
+        cp ../../templates/default.desktop .
+        echo "Couldn't find any pixmaps, copying generic 256x256 icon into pixmaps/"
+        mkdir pixmaps
+        cp ../../templates/shttr.png pixmaps/shttr.png
+    fi
 }
 
 _list() {
-    local entries="$(ls $SHTTR_HOME)"
-    for entry in $(ls $SHTTR_HOME); do
+    for entry in $(ls $SHTTR_HOME/pkgs | grep -vi templates); do
         printf "$entry"
         if [[ -e ~/.local/bin/$entry ]]; then
             printf " [installed]"
@@ -82,11 +127,11 @@ __make_operation() {
     if [[ -z "$SHTTR_PACKAGE_NAME" ]]; then
         echo "Error: nothing to $OPERATION!"
         exit -2  # TODO figure out if this is the correct error code
-    elif [[ ! -d "$SHTTR_HOME/$SHTTR_PACKAGE_NAME" ]]; then
+    elif [[ ! -d "$SHTTR_HOME/pkgs/$SHTTR_PACKAGE_NAME" ]]; then
         echo "Error: package '$SHTTR_PACKAGE_NAME' not found!"
         exit -2
     fi
-    cd "$SHTTR_HOME/$SHTTR_PACKAGE_NAME"
+    cd "$SHTTR_HOME/pkgs/$SHTTR_PACKAGE_NAME"
     if [[ ! -e shttr.conf ]]; then
         echo "Error: shttr.conf not found for package '$SHTTR_PACKAGE_NAME'!"
         exit -2
@@ -97,7 +142,7 @@ __make_operation() {
 }
 
 # Argument parsing
-if [[ -z "$1" ]]; then
+if [[ $# -lt 1 ]]; then
     display_help
     exit -1
 elif [[ "help" =~ "$1" || "-h" == "$1" ]]; then
