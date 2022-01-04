69732ec07e10855234b5a4f4265dfb9fb9117eaa
Chandler DeLoach <deloach.cdd@gmail.com>
2022-01-03 20:05:45 -0700

    evilmacs gets a path in .config

--- emacs/hooks.sh
+++ emacs/hooks.sh
@@ -6,4 +6,4 @@ cd ..
 link_config chemacs2 ~/.emacs.d
 link_config config/emacs-profiles.el ~/.emacs-profiles.el
 link_config config/doom ~/.config/doom
-
+link_config config/evilmacs ~/.config/emacs
