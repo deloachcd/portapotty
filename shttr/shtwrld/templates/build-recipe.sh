bbe9295b095efdfe452d15dc4d0f64fcc492a9cc
Chandler DeLoach <deloach.cdd@gmail.com>
2022-01-05 15:40:51 -0700

    update shttr template files for new Makefile and new shttr.conf

--- shttr/shtwrld/templates/build-recipe.sh
+++ shttr/shtwrld/templates/build-recipe.sh
@@ -3,23 +3,23 @@ set -euo pipefail
 
 shttr_build() {
     local MAKEOPTS="-j$(nproc)"
-    local GIT_REMOTE="" # TODO set this
+    local GIT_REMOTE=""  # TODO add the git repo URL here
     local REPO_DIR="repo"
+    local SHTTR_DIR=$PWD
 
     if [[ ! -d "$REPO_DIR" ]]; then
         git clone "$GIT_REMOTE" "$REPO_DIR"
     fi
 
-    # TODO script out project build instructions here,
-    # before the project gets made
-
+    # TODO add instructions for building the project here
     make $MAKEOPTS
 }
 
 # resolve dependencies
-if [[ "$SHTTR_APT_DEPENDENCIES" == "build-dep" ]]; then
-    sudo apt build-dep "$SHTTR_APP_NAME"
-elif [[ ! -z "$SHTTR_APT_DEPENDENCIES" ]]; then
+if [[ ! -z "$SHTTR_BUILD_DEP" ]]; then
+    sudo apt build-dep "$SHTTR_PACKAGE_NAME"
+fi
+if [[ ! -z "$SHTTR_APT_DEPENDENCIES" ]]; then
     sudo apt install $SHTTR_APT_DEPENDENCIES
 fi
 
