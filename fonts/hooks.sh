e4cc7f0c57e58ec7f1dbc697f63ba6745b899961
Chandler DeLoach <deloach.cdd@gmail.com>
2022-01-06 14:17:41 -0700

    update font deploy for macos

--- fonts/hooks.sh
+++ fonts/hooks.sh
@@ -1,3 +1,8 @@
-ensure_dir_exists ~/.fonts
-link_config bundle ~/.fonts/potty
-fc-cache -fv
+if [[ $UNAME == linux ]]; then
+    ensure_dir_exists ~/.fonts
+    link_config bundle ~/.fonts/potty
+    fc-cache -fv
+elif [[ $UNAME == macos ]]; then
+    ensure_dir_exists ~/Library/Fonts
+    link_config bundle ~/Library/Fonts/potty
+fi
