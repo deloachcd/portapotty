6064083e1c951e2668b8d8be93b03a94a716cfb2
Chandler DeLoach <deloach.cdd@gmail.com>
2022-01-06 14:19:43 -0700

    skip shttr install on macos

--- shttr/hooks.sh
+++ shttr/hooks.sh
@@ -1,2 +1,4 @@
-link_config bin/shttr.sh ~/.local/bin/shttr
-link_config shtwrld ~/.local/share/shttr
+if [[ $UNAME == linux ]]; then
+    link_config bin/shttr.sh ~/.local/bin/shttr
+    link_config shtwrld ~/.local/share/shttr
+fi
