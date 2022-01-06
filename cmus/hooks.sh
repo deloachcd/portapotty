90574814d126f01ea88c03bbb7e596d1fccdb94d
Chandler DeLoach <deloach.cdd@gmail.com>
2022-01-06 14:16:52 -0700

    modify cmus hoosk for macos

--- cmus/hooks.sh
+++ cmus/hooks.sh
@@ -1,3 +1,6 @@
 ensure_dir_exists ~/.config/cmus
-link_config desktop/cmus.desktop ~/.local/share/applications/cmus.desktop
 link_config config/rc ~/.config/cmus/rc
+
+if [[ $UNAME == linux ]]; then
+    link_config desktop/cmus.desktop ~/.local/share/applications/cmus.desktop
+fi
