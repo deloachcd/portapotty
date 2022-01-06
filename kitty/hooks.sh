db68567feda7bdc4bd3a6595204708a4edc2b098
Chandler DeLoach <deloach.cdd@gmail.com>
2022-01-06 14:18:23 -0700

    skip kitty hooks on macos

--- kitty/hooks.sh
+++ kitty/hooks.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
-
-ensure_dir_exists ~/.config/kitty
-link_config config/kitty.conf ~/.config/kitty/kitty.conf
+if [[ $UNAME == linux ]]; then
+    ensure_dir_exists ~/.config/kitty
+    link_config config/kitty.conf ~/.config/kitty/kitty.conf
+fi
