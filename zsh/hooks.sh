45e556192a502d97ffe4f16886b2971e4ec8a316
Chandler DeLoach <deloach.cdd@gmail.com>
2022-01-06 14:19:56 -0700

    modify zsh hooks for macos

--- zsh/hooks.sh
+++ zsh/hooks.sh
@@ -1,7 +1,9 @@
 link_config home/zshrc ~/.zshrc
 link_config config ~/.config/zsh
 
-# change user's shell to zsh if it's bash
-if cat /etc/passwd | grep $(whoami) | grep bash 2>&1 1>/dev/null; then
-    sudo chsh -s /bin/zsh $(whoami)
+if [[ $UNAME == linux ]]; then
+    # change user's shell to zsh if it's bash
+    if cat /etc/passwd | grep $(whoami) | grep bash 2>&1 1>/dev/null; then
+        sudo chsh -s /bin/zsh $(whoami)
+    fi
 fi
