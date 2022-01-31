67100d79e839845f92833d3d6273407cfcd600b5
Chandler DeLoach <deloach.cdd@gmail.com>
2022-01-31 15:18:56 -0700

    update rofi hooks to deploy modified theme on hidpi display

--- rofi/hooks.sh
+++ rofi/hooks.sh
@@ -10,5 +10,12 @@ if [[ $UNAME == linux ]]; then
         halting_message "$message_buffer"
         unset message_buffer
     fi
-    link_config config ~/.config/rofi
+    ensure_dir_exists ~/.config/rofi
+    RESOLUTION=$(xrandr | grep '*' | head -n 1 | awk '{ printf $1 }')
+    if [[ $RESOLUTION == '3840x2160' ]]; then
+        link_config config/simple-light-hidpi.rasi ~/.config/rofi/simple-light.rasi 
+    else
+        link_config config/simple-light.rasi ~/.config/rofi/simple-light.rasi 
+    fi
+    link_config config/config.rasi ~/.config/rofi/config.rasi 
 fi
