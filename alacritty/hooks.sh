249e38e8c3ec8086c868895496299870cedbba29
Chandler DeLoach <deloach.cdd@gmail.com>
2022-03-09 22:48:54 -0700

    move things around ahead of script behavior change (breaking)

--- alacritty/hooks.sh
+++
@@ -1,4 +0,0 @@
-if [[ $UNAME == linux ]]; then
-    ensure_dir_exists ~/.config/alacritty
-    link_config config/alacritty.yml ~/.config/alacritty/alacritty.yml
-fi
--- xbindkeys/hooks.sh
+++
@@ -1,2 +0,0 @@
-link_config config/xbindkeysrc ~/.xbindkeysrc
-link_config config/xbindkeys.desktop  ~/.config/autostart/xbindkeys.desktop 
