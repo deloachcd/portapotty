2bf0be4195d038c5d50c333b86673f72d9af7047
Chandler DeLoach <deloach.cdd@gmail.com>
2022-03-14 18:53:01 -0600

    emacs layer now deploys project seperately

--- base/emacs/hooks.sh
+++ base/emacs/hooks.sh
@@ -1,9 +1,3 @@
-cd config
-git submodule init
-git submodule update
-cd ..
-
-link_config chemacs2 ~/.emacs.d
-link_config config/emacs-profiles.el ~/.emacs-profiles.el
-link_config config/doom ~/.config/doom
-link_config config/evilmacs ~/.config/emacs
+if [[ ! -d ~/.emacs.d ]]; then
+   git clone git@github.com:deloachcd/emacs.git
+fi
