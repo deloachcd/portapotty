98b7ede4c8dbad3867de94f8f8b0f50ea7912dbd
Jimmy Wilson <jwilson@Jimmys-MBP.hsd1.co.comcast.net>
2022-03-15 17:24:23 -0600

    successfully setup emacs with vterm and nativecomp on macos

--- base/emacs/hooks.sh
+++ base/emacs/hooks.sh
@@ -1,3 +1,17 @@
 if [[ ! -d ~/.emacs.d ]]; then
    git clone git@github.com:deloachcd/emacs.git ~/.emacs.d
 fi
+
+if [[ $UNAME == darwin && $INSTALL_PACKAGES == true ]]; then
+    EMACS_FORMULA="emacs-plus@28"
+    EMACS_OPTIONS="--with-native-comp"
+    echo
+    uline_echo "The following formula will be installed:"
+    echo "$EMACS_FORMULA"
+    echo
+    brew tap d12frosted/emacs-plus
+    brew install $EMACS_FORMULA $EMACS_OPTIONS
+    if [[ ! -e /Applications/Emacs.app ]]; then
+        ln -s /usr/local/opt/$EMACS_FORMULA/Emacs.app /Applications
+    fi
+fi
