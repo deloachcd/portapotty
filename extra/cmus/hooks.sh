31f97ce4cba2afad3b0b22bab153ddb9b384aa1d
Chandler DeLoach <deloach.cdd@gmail.com>
2022-03-15 11:43:36 -0600

    deploy dpp theme

--- extra/cmus/hooks.sh
+++ extra/cmus/hooks.sh
@@ -1,5 +1,6 @@
 ensure_dir_exists ~/.config/cmus
 link_config config/rc ~/.config/cmus/rc
+link_config config/dpp.theme ~/.config/cmus/dpp.theme
 
 if [[ $UNAME == linux ]]; then
     link_config desktop/cmus.desktop ~/.local/share/applications/cmus.desktop
