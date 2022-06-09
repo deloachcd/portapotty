1f18e8f8815378a90ed4dcca1b488315336d2857
Chandler DeLoach <deloach.cdd@gmail.com>
2022-06-09 17:50:46 -0600

    ensure user-installed packages from pip are in user's PATH (macos)

--- base/zsh/config/path.sh
+++ base/zsh/config/path.sh
@@ -1,4 +1,9 @@
 if [[ -d "$HOME/.local/bin" && \
-	! "$PATH" == *"$HOME/.local/bin"* ]] ; then
+        ! "$PATH" == *"$HOME/.local/bin"* ]] ; then
     PATH="$HOME/.local/bin:$PATH"
+fi
+
+if [[ $(uname) == 'Darwin' && \
+        ! "$PATH" == *"$HOME/Library/Python/3.9/bin"* ]]; then
+    PATH="$HOME/Library/Python/3.9/bin:$PATH"
 fi
